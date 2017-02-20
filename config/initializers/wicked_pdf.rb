bd7e9cf20eac00aa7e1baa50c4d53623061edc58
Suhas Ramesh <suhasr94@gmail.com>
2017-02-20 04:36:14 +0000

    Tata image added

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -13,10 +13,10 @@ WickedPdf.config = {
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+   exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
-   exe_path: '/usr/local/bin/wkhtmltopdf'
+   #exe_path: '/usr/local/bin/wkhtmltopdf'
 }
